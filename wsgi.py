789a7d99fde58118443186c35f2a43ed55f47d04
simon-topp <52258753+simon-topp@users.noreply.github.com>
2019-06-27 22:46:56 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,15 @@ application = Flask(__name__)
 
 @application.route("/")
 def helloJoe():
-    return "Hello Joe!"
+    return "Hello Joey!"
 
 @application.route("/bob")
 def helloBob():
     return "Hello Bob!!!"
+
+@application.route("/bob2")
+def helloBob():
+    return "Hello Bob2!!!"
 
 if __name__ == "__main__":
     application.run()
