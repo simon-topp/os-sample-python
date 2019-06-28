27193b7f8077267d47526a240041ca65a3413952
simon-topp <simon.topp@gmail.com>
2019-06-28 22:30:50 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,6 +12,11 @@ def helloBob():
   print("helloBob()")
   return "Hello Bob !!"
 
+@application.route("/bob1")
+def helloBob1():
+  print("helloBob1()")
+  return "Hello Bob !1!"
+
 @application.route("/bob2")
 def helloBob2():
   print("helloBob2()")
