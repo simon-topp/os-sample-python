0acaa574e917e7dd454618055459fc5635ae5c56
simon-topp <simon.topp@gmail.com>
2019-07-03 17:47:07 +0200

    new

--- wsgi.py
+++ wsgi.py
@@ -5,22 +5,22 @@ application = Flask(__name__)
 @application.route("/")
 def helloJoe():
   print("helloJoe()")
-  return "Hello Joe !"
+  return "Hello Joe 33!"
 
 @application.route("/bob")
 def helloBob():
   print("helloBob()")
-  return "Hello Bob !!"
+  return "Hello Bob 33!!"
 
 @application.route("/bob1")
 def helloBob1():
   print("helloBob1()")
-  return "Hello Bob !1!"
+  return "Hello Bob 33 !1!"
 
 @application.route("/bob2")
 def helloBob2():
   print("helloBob2()")
-  return "Hello Bob !2!"
+  return "Hello Bob 33 !2!"
 
 if __name__ == "__main__":
   application.run()
