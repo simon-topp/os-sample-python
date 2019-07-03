553936133d13135f94304f5f6293277a153c1465
simon-topp <simon.topp@gmail.com>
2019-07-03 21:45:38 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,31 +1,31 @@
 import flask, os
 
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
-@app.route('/favicon.ico')
+@application.route('/favicon.ico')
 def favicon():
   print("favicon()")
-  return flask.send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+  return flask.send_from_directory(os.path.join(application.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route("/")
+@application.route("/")
 def helloJoe():
   print("helloJoe()")
   return "Hello Joe 33!"
 
-@app.route("/bob")
+@application.route("/bob")
 def helloBob():
   print("helloBob()")
   return "Hello Bob 33!!"
 
-@app.route("/bob1")
+@application.route("/bob1")
 def helloBob1():
   print("helloBob1()")
   return "Hello Bob 33 !1!"
 
-@app.route("/bob2")
+@application.route("/bob2")
 def helloBob2():
   print("helloBob2()")
   return "Hello Bob 33 !2!"
 
 if __name__ == "__main__":
-  app.run()
+  application.run()
